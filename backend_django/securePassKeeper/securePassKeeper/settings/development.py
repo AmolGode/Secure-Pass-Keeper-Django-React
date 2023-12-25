e00b85a1588dbf9821f58d02b5e61b1ddd6599b0
Amol Gode <17amolgode@gmail.com>
2023-12-25 21:08:15 +0530

    edit allow host

--- backend_django/securePassKeeper/securePassKeeper/settings/development.py
+++ backend_django/securePassKeeper/securePassKeeper/settings/development.py
@@ -10,7 +10,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY') #TODO
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True 
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["*"]# "127.0.0.1"
 
 CORS_ALLOWED_ORIGINS = ['http://localhost:5173']
 
