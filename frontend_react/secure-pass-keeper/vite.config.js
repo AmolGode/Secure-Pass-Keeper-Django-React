72be33e7f6a14bb2e33576f00b2ccb8fb2b57ae0
Amol Gode <17amolgode@gmail.com>
2023-12-25 23:16:34 +0530

    fising base url issue

--- frontend_react/secure-pass-keeper/vite.config.js
+++ frontend_react/secure-pass-keeper/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'import.meta.env.VITE_API_BASE_URL': JSON.stringify(process.env.REACT_APP_API_BASE_URL),
+  },
 })
