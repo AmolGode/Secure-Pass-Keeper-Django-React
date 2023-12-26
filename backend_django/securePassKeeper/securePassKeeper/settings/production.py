7673a2ecbbb80390ad0814eb826cb32500011741
Amol Gode <17amolgode@gmail.com>
2023-12-27 00:40:45 +0530

    setup prod setting

---
+++ backend_django/securePassKeeper/securePassKeeper/settings/production.py
@@ -0,0 +1,29 @@
+from .base import *
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True 
+
+ALLOWED_HOSTS = ["16.170.60.144"]# "127.0.0.1"
+
+CORS_ALLOWED_ORIGINS = ['http://16.170.60.144:5173']
+
+
+# Database
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("POSTGRES_DB"),
+        "USER": os.environ.get("POSTGRES_USER"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+        "HOST": "db",
+        "PORT": "5432",
+    }
+}
