22be3711e1206c0656b419a39b7fa6bd7f0ba0bd
Amol Gode <17amolgode@gmail.com>
2023-12-26 23:41:09 +0530

    use env for secrets

--- backend_django/securePassKeeper/securePassKeeper/settings/development.py
+++ backend_django/securePassKeeper/securePassKeeper/settings/development.py
@@ -12,7 +12,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]# "127.0.0.1"
 
-CORS_ALLOWED_ORIGINS = ['http://51.20.122.213:5173/', 'http://localhost:5173']
+CORS_ALLOWED_ORIGINS = ['http://51.20.122.213:5173', 'http://localhost:5173']
 
 
 # Database
@@ -28,9 +28,9 @@ CORS_ALLOWED_ORIGINS = ['http://51.20.122.213:5173/', 'http://localhost:5173']
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ.get("DB_NAME"),
-        "USER": os.environ.get("DB_USER"),
-        "PASSWORD": os.environ.get("DB_PASSWORD"),
+        "NAME": os.environ.get("POSTGRES_DB"),
+        "USER": os.environ.get("POSTGRES_USER"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
         "HOST": "db",
         "PORT": "5432",
     }
--- frontend_react/secure-pass-keeper/vite.config.js
+++ frontend_react/secure-pass-keeper/vite.config.js
@@ -1,10 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  define: {
-    'import.meta.env.VITE_API_BASE_URL': JSON.stringify(process.env.REACT_APP_API_BASE_URL),
-  },
-})
+  plugins: [react()]
+});
